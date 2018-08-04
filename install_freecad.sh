73380ebe011dfa9fafa964040bd07d08f641fe53
Hrant Manasyan <manasyanhrant@gmail.com>
2018-08-04 10:40:56 +0400

    Initial version of installation script.

---
+++ install_freecad.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+check_for_error(){
+	RED='\033[0;31m'
+	NC='\033[0m'
+	if [ $1 -ne "0" ]; then
+		echo -e "${RED}Error $2. ${NC}\n"
+		exit 1;
+	fi
+}
+
+install_prerequisites(){
+	sudo apt install build-essential cmake python python-matplotlib
+	libtool libcoin80-dev libsoqt4-dev libxerces-c-dev libboost-dev
+	libboost-filesystem-dev libboost-regex-dev
+	libboost-program-options-dev libboost-signals-dev libboost-thread-dev
+	libboost-python-dev libqt4-dev libqt4-opengl-dev qt4-dev-tools
+	python-dev python-pyside pyside-tools libeigen3-dev libqtwebkit-dev
+	libshiboken-dev libpyside-dev libode-dev swig libzipios++-dev
+	libfreetype6-dev liboce-foundation-dev liboce-modeling-dev
+	liboce-ocaf-dev liboce-visualization-dev liboce-ocaf-lite-dev
+	libsimage-dev checkinstall python-pivy python-qt4 doxygen libspnav-dev
+	oce-draw liboce-foundation-dev liboce-modeling-dev liboce-ocaf-dev
+	liboce-ocaf-lite-dev liboce-visualization-dev libmedc-dev libvtk6-dev
+	libproj-dev 
+	check_for_error $? "Installing prerequsites"
+}
+
+build_freecad(){
+	cd FreeCAD
+	cmake -DFREECAD_USE_EXTERNAL_PIVY=1 -DCMAKE_BUILD_TYPE=Release .
+	check_for_error $? "Configuration failed"
+	make -j$(nproc)
+	check_for_error $? "Compilation failed"
+	cd -
+}
