24b1aeeac2f6eb467de9db6ec7f87c45464716f8
Hrant Manasyan <manasyanhrant@gmail.com>
2018-08-04 21:11:41 +0400

    Added submodules. Updated installer script.

--- install_freecad.sh
+++ install_freecad.sh
@@ -10,18 +10,18 @@ check_for_error(){
 }
 
 install_prerequisites(){
-	sudo apt install build-essential cmake python python-matplotlib
-	libtool libcoin80-dev libsoqt4-dev libxerces-c-dev libboost-dev
-	libboost-filesystem-dev libboost-regex-dev
-	libboost-program-options-dev libboost-signals-dev libboost-thread-dev
-	libboost-python-dev libqt4-dev libqt4-opengl-dev qt4-dev-tools
-	python-dev python-pyside pyside-tools libeigen3-dev libqtwebkit-dev
-	libshiboken-dev libpyside-dev libode-dev swig libzipios++-dev
-	libfreetype6-dev liboce-foundation-dev liboce-modeling-dev
-	liboce-ocaf-dev liboce-visualization-dev liboce-ocaf-lite-dev
-	libsimage-dev checkinstall python-pivy python-qt4 doxygen libspnav-dev
-	oce-draw liboce-foundation-dev liboce-modeling-dev liboce-ocaf-dev
-	liboce-ocaf-lite-dev liboce-visualization-dev libmedc-dev libvtk6-dev
+	sudo apt install -y build-essential cmake python python-matplotlib \
+	libtool libcoin80-dev libsoqt4-dev libxerces-c-dev libboost-dev \
+	libboost-filesystem-dev libboost-regex-dev \
+	libboost-program-options-dev libboost-signals-dev libboost-thread-dev \
+	libboost-python-dev libqt4-dev libqt4-opengl-dev qt4-dev-tools \
+	python-dev python-pyside pyside-tools libeigen3-dev libqtwebkit-dev \
+	libshiboken-dev libpyside-dev libode-dev swig libzipios++-dev \
+	libfreetype6-dev liboce-foundation-dev liboce-modeling-dev \
+	liboce-ocaf-dev liboce-visualization-dev liboce-ocaf-lite-dev \
+	libsimage-dev checkinstall python-pivy python-qt4 doxygen libspnav-dev \
+	oce-draw liboce-foundation-dev liboce-modeling-dev liboce-ocaf-dev \
+	liboce-ocaf-lite-dev liboce-visualization-dev libmedc-dev libvtk6-dev \
 	libproj-dev 
 	check_for_error $? "Installing prerequsites"
 }
@@ -32,5 +32,14 @@ build_freecad(){
 	check_for_error $? "Configuration failed"
 	make -j$(nproc)
 	check_for_error $? "Compilation failed"
+	sudo make install
+	check_for_error $? "Installation failed"
 	cd -
 }
+
+main(){
+	install_prerequisites
+	build_freecad
+}
+
+main
